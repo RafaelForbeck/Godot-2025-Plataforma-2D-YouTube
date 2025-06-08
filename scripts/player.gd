f593ae0c83efb256690ae9e76f716088b7a7bb33
RafaelForbeck <rafaelforbeck@gmail.com>
2025-06-08 18:00:24 -0300

    Gerencia colisão entre projétil e player

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,8 @@ enum PlayerState {
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var hitbox_collision_shape: CollisionShape2D = $Hitbox/CollisionShape2D
+
 @onready var reload_timer: Timer = $ReloadTimer
 
 @export var max_speed = 180.0
@@ -91,7 +93,7 @@ func exit_from_slide_state():
 func go_to_hurt_state():
 	status = PlayerState.hurt
 	anim.play("hurt")
-	velocity = Vector2.ZERO
+	velocity.x = 0
 	reload_timer.start()
 
 func idle_state(delta):
@@ -200,12 +202,24 @@ func set_small_collider():
 	collision_shape.shape.height = 10
 	collision_shape.position.y = 3
 	
+	hitbox_collision_shape.shape.size.y = 10
+	hitbox_collision_shape.position.y = 3
+	
 func set_large_collider():
 	collision_shape.shape.radius = 6
 	collision_shape.shape.height = 16
 	collision_shape.position.y = 0
+	
+	hitbox_collision_shape.shape.size.y = 15
+	hitbox_collision_shape.position.y = 0.5
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Enemies"):
+		hit_enemy(area)
+	elif area.is_in_group("LethalArea"):
+		hit_lethal_area()
+
+func hit_enemy(area: Area2D):
 	if velocity.y > 0:
 		# inimigo morre
 		area.get_parent().take_damage()
@@ -214,6 +228,9 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 		# player morre
 		if status != PlayerState.hurt:
 			go_to_hurt_state()
+	
+func hit_lethal_area():
+	go_to_hurt_state()
 
 func _on_reload_timer_timeout() -> void:
 	get_tree().reload_current_scene()
